8d9b032d16d4cddf55634a7d28015613fd81bf7e
logixkjy <logixkjy@naver.com>
2026-01-16 18:26:29 +0900

    [init] 프로젝트 생성

---
+++ MovieSearchApp/MovieSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchAppApp.swift
+//  MovieSearchApp
+//
+//  Created by JooYoung Kim on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
